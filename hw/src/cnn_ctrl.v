0952f5ae0a32d5b7847b750038e37c1c5203f702
이상우 <rain030411@snu.ac.kr>
2025-07-19 17:53:20 +0900

    now fixing

--- hw/src/cnn_ctrl.v
+++ hw/src/cnn_ctrl.v
@@ -7,9 +7,10 @@ module cnn_ctrl #(
     parameter W_CHANNEL     = `W_CHANNEL,
     parameter W_FRAME_SIZE  = `W_FRAME_SIZE,
     parameter W_DELAY       = `W_DELAY,
+    parameter N_PESYNC      = `N_PESYNC,
     
     parameter IFM_BUF_CNT   = `IFM_BUFFER_CNT,      // 4
-    parameter W_IFM_BUF     = `W_IFM_BUFFER         // 2
+    parameter W_IFM_BUF     = `IFM_BUFFER           // 2
 )(
     input  wire                     clk,
     input  wire                     rstn,
@@ -30,6 +31,12 @@ module cnn_ctrl #(
     output wire                     o_ctrl_hsync_run,
     output wire [W_DELAY-1:0]       o_ctrl_hsync_cnt,
     output wire                     o_ctrl_data_run,
+    
+    output wire                     o_is_first_row,
+    output wire                     o_is_last_row,
+    output wire                     o_is_first_col,
+    output wire                     o_is_last_col,
+    
     output wire [W_SIZE-1:0]        o_row,
     output wire [W_SIZE-1:0]        o_col,
     output wire [W_CHANNEL-1:0]     o_chn,
@@ -38,12 +45,15 @@ module cnn_ctrl #(
 );
 
 // FSM states
-localparam ST_IDLE   = 2'b00,
-           ST_VSYNC  = 2'b01,
-           ST_HSYNC  = 2'b10,
-           ST_DATA   = 2'b11;
+localparam ST_IDLE   = 3'd0,
+           ST_VSYNC  = 3'd1,
+           ST_HSYNC  = 3'd2,
+           ST_PESYNC = 3'd3,
+           ST_DATA   = 3'd4;
 
-reg [1:0]  cstate, nstate;
+
+reg [2:0]  cstate, nstate;
+reg        ctrl_pesync_cnt;     // log2(N_PESYNC)=1
 reg        ctrl_vsync_run;
 reg [W_DELAY-1:0] ctrl_vsync_cnt;
 reg        ctrl_hsync_run;
@@ -54,15 +64,30 @@ reg [W_SIZE-1:0]        row;
 reg [W_SIZE-1:0]        col;
 reg [W_CHANNEL-1:0]     chn; 
 reg [W_FRAME_SIZE-1:0]  data_count;
-wire                    end_frame;
+
+wire                    end_frame = (data_count == q_frame_size - 1);
 
 wire [W_IFM_BUF-1:0]    next_buf_id = (row + 1'b1);
 
+wire                    is_first_row = (row == 0) ? 1'b1: 1'b0;
+wire                    is_last_row  = (row == q_height-1) ? 1'b1: 1'b0;
+wire                    is_first_col = (col == 0) ? 1'b1: 1'b0;
+wire                    is_last_col  = (col == q_width-1) ? 1'b1 : 1'b0;
 
 // State register
 always @(posedge clk or negedge rstn) begin
     if (!rstn) cstate <= ST_IDLE;
     else       cstate <= nstate;
+end
+
+// PESYNC counter
+always @(posedge clk or negedge rstn) begin
+    if (!rstn)
+        pesync_cnt <= 0;
+    else if (cstate == ST_PESYNC)
+        pesync_cnt <= pesync_cnt + 1;
+    else
+        pesync_cnt <= 0;
 end
 
 // Next-state logic
@@ -73,10 +98,9 @@ always @(*) begin
         ST_VSYNC: 
             nstate = q_filter_buf_done ? ST_HSYNC : ST_VSYNC;
         ST_HSYNC: 
-            nstate = 
-                (row == q_height-1) ? ST_DATA :
-                q_ifm_buf_done[next_buf_id] ? ST_DATA : 
-                ST_HSYNC;
+            nstate = is_last_row || q_ifm_buf_done[next_buf_id] ? ST_DATA : ST_HSYNC;
+        ST_PESYNC:
+            nstate = (pesync_cnt == N_PESYNC-1) ? ST_DATA : ST_PESYNC;
         ST_DATA:  
             nstate = 
                 end_frame ? ST_IDLE : 
@@ -116,42 +140,31 @@ always @(posedge clk or negedge rstn) begin
     end
 end
 
-// (tiled!! input) channel counter
-always @(posedge clk or negedge rstn) begin
-    if (!rstn) begin
-        chn <= 0;
-    end else if (ctrl_data_run) begin
-        if (chn == q_channel - 1)
-            chn <= 0;
-        else
-            chn <= chn + 1;
-    end
-end
 
-// row & col counter
+// Nested counters: col fastest, then chn, then row
 always @(posedge clk or negedge rstn) begin
     if (!rstn) begin
         row <= 0;
+        chn <= 0;
         col <= 0;
-    // row & col update only when last channel 
-    end else if (ctrl_data_run && chn == q_channel - 1) begin
-        // last column
-        if (col == q_width - 1) begin
-            // end of frame
-            if (end_frame) begin
-                row <= 0;
-                col <= 0;
-            // next row
-            end else begin
-                row <= row + 1;
-                col <= 0;
-            end
-        // not last column
-        end else begin
+    end else if (ctrl_data_run) begin
+        if (col != q_width - 1) begin
             col <= col + 1;
+        end else begin
+            col <= 0;
+            if (chn != q_channel - 1) begin
+                chn <= chn + 1;
+            end else begin
+                chn <= 0;
+                if (row != q_height - 1)
+                    row <= row + 1;
+                else
+                    row <= 0;
+            end
         end
     end
 end
+
 
 // Data count (include channel, actually the number of tiles)
 always @(posedge clk or negedge rstn) begin
@@ -165,9 +178,6 @@ always @(posedge clk or negedge rstn) begin
     end
 end
 
-// End of frame signal
-assign end_frame = (data_count == q_frame_size - 1);
-
 // Outputs
 assign o_ctrl_vsync_run = ctrl_vsync_run;
 assign o_ctrl_vsync_cnt = ctrl_vsync_cnt;
@@ -179,5 +189,10 @@ assign o_col            = col;
 assign o_chn            = chn;
 assign o_data_count     = data_count;
 assign o_end_frame      = end_frame;
+
+assign o_is_first_row   = is_first_row;
+assign o_is_last_row    = is_last_row;
+assign o_is_first_col   = is_first_col;
+assign o_is_last_col    = is_last_col;
 
 endmodule
